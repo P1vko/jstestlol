76636f59e3207a780c554d9a19d6870281ee5149
P1vko <60178690+P1vko@users.noreply.github.com>
2021-03-14 18:56:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const tabs = document.getElementById('tabs');
+const contents = document.getElementById('contents');
+
+const changeClass = el => {
+    for(let i = 0; i < tabs.children.length; i++) {
+        tabs.children[i].classList.remove('active');
+    }
+    el.classList.add('active');
+}
+
+tabs.addEventListener('click', e => {
+    const currTab = e.target.dataset.btn;
+    changeClass(e.target);
+    for(let i = 0; i < contents.children.length; i++) {
+        contents.children[i].classList.remove('active');
+        if(contents.children[i].dataset.content == currTab) {
+            contents.children[i].classList.add('active');
+        }
+    } 
+});
